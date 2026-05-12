ead0f20b4351c8f1f6e6bb82602bb23d66e7448d
unperturbable-se <raedovais321@gmail.com>
2026-05-12 10:21:35 +0500

    make login screen more responsive add loading bar in the beginning to give time for test to load the button now hovers so user can know that app is working pressing enter can also allow login

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -1,30 +1,41 @@
 import 'package:flutter/material.dart';
 
-class MyButton extends StatelessWidget{
+class MyButton extends StatefulWidget {
   final void Function()? onTap;
   final String text;
-  const MyButton({
-    super.key,
-    required this.text,
-    required this.onTap,
-  });
+
+  const MyButton({super.key, required this.text, required this.onTap});
+
+  @override
+  State<MyButton> createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  bool _hovered = false;
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.secondary,
-          borderRadius: BorderRadius.circular(8)
-
-        ),
-        padding: EdgeInsets.all(25),
-        margin: const EdgeInsets.symmetric(horizontal: 25),
-        child: Center(
-          child: Text(text),
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hovered = true),
+      onExit: (_) => setState(() => _hovered = false),
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 150),
+          decoration: BoxDecoration(
+            color: _hovered
+                ? Theme.of(context).colorScheme.primary
+                : Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(8),
+            boxShadow: _hovered
+                ? [const BoxShadow(color: Colors.black26, blurRadius: 8, offset: Offset(0, 4))]
+                : [],
+          ),
+          padding: const EdgeInsets.all(25),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          child: Center(child: Text(widget.text)),
         ),
       ),
     );
   }
-
 }
