8ea83d67fd08da12cc45e7f51e56f94dcdf3b25a
unperturbable-se <raedovais321@gmail.com>
2026-05-12 09:55:44 +0500

    add a friends tab add an option to communicate with friends(ie by linking to the main chat page) gave every user a unique contact key

---
+++ lib/supabase_options.dart
@@ -0,0 +1,5 @@
+class SupabaseOptions 
+{
+  static const String url = 'https://qmytmmnswvwfdygyxssh.supabase.co';
+  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFteXRtbW5zd3Z3ZmR5Z3l4c3NoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0MjMzNjYsImV4cCI6MjA5Mzk5OTM2Nn0.TbUdGtFQlHLDzfXHrq8jj8mjIxBHQXtMVLnL3CT_rsk';
+}
